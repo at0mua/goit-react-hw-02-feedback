465e76cfe32b0fbb53586522bd4c328590124ca0
At0mUA <zatom89@gmail.com>
2020-09-03 11:43:44 +0300

    Updates

---
+++ precache-manifest.6f9e3c88f27dd113065b66988ea01f67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f1fb7aa204aacf184af54c58cdc7bb",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "110e04504c0938e35a45",
+    "url": "/goit-react-hw-02-feedback/static/js/2.eb03c0e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.eb03c0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8dbc032c95cb099b3ad",
+    "url": "/goit-react-hw-02-feedback/static/js/main.e1e90bdd.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
