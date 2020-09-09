b6db3942d966294beca7e8575db090624e93161c
At0mUA <zatom89@gmail.com>
2020-09-09 11:25:07 +0300

    fix key

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,14 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import StatisticsItem from "./StatisticsItem";
-import createId from "../../util/createId";
 import s from "./Statistics.module.scss";
 
 const Statistic = ({ stats, totalFeedback, positivePercentage }) => {
   return (
     <ul className={s.statistic_list}>
       {stats.map((stat) => (
-        <StatisticsItem key={createId()} name={stat[0]} value={stat[1]} />
+        <StatisticsItem key={stat[0]} name={stat[0]} value={stat[1]} />
       ))}
       <StatisticsItem name="total: " value={totalFeedback} />
       <StatisticsItem
